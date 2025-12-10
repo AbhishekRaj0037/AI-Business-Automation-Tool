194d57f497c3d0c07afa29f691ab1d3317bba6d4
Abhishek Raj <abhishekraj@192.168.1.3>
2025-12-10 23:56:37 +0530

    Added Complete logic for File Download in local for the App

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import imaplib
 import email
+import os
 
 app=FastAPI()
 
@@ -18,19 +19,26 @@ mail.login(username,password)
 def read_root():
     mail.select("inbox")
     status,messages=mail.search(None,"ALL")
-    msg=mail.fetch('28777','RFC822')
-    
-    raw=msg[1][0][1]
-    raw_message=raw.decode('utf-8')
-    result=email.message_from_string(raw_message)
-    if result.is_multipart():
-        print("YES")
-    else:
-        print("No")
-    breakpoint()
-    for part in finalMsg.walk():
-        if part.get_content_maintype() == 'application/pdf':
-            print("Yes this part is pdf")
+    messages=messages[0].decode('utf-8')
+    messages=messages.split()
+    for ele in messages:
+        msg=mail.fetch(ele,'RFC822')
+        raw=msg[1][0][1]
+        raw_message=email.message_from_bytes(raw)
+        # result=email.message_from_string(raw_message)
+        # if result.is_multipart():
+        #     print("YES")
+        # else:
+        #     print("No")
+        for part in raw_message.walk():
+            if part.get_content_maintype() == 'multipart' or part.get('Content-Disposition') is None:
+                continue
+            filename=part.get_filename()
+            if bool(filename):
+                filePath =os.path.join('/Users/abhishekraj/desktop/AI Business Automation Assistant/Downloaded Files',filename)
+                with open(filePath,'wb') as f:
+                    f.write(part.get_payload(decode=True))
+                break
 
 
    
