c1d9898a09a2c4545ce4ac0e97d2829d17006e44
Abhishek Raj <77464347+AbhishekRaj0037@users.noreply.github.com>
2025-12-14 00:15:24 +0530

    Added .env support

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 import imaplib
 import email
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+
 
 app=FastAPI()
 
-imap_server="imap.gmail.com"
-username="mabhishek.tethered@gmail.com"
-password="sobu ofoa gqxm cqjt"
+imap_server=os.getenv("imap_server")
+username=os.getenv("username")
+password=os.getenv("password")
 
 mail= imaplib.IMAP4_SSL(imap_server)
 
@@ -18,18 +23,15 @@ mail.login(username,password)
 @app.get("/")
 def read_root():
     mail.select("inbox")
-    status,messages=mail.search(None,"ALL")
+    # status,messages=mail.search(None,"ALL")
+    status,messages=mail.uid('search', None, 'UNSEEN')
     messages=messages[0].decode('utf-8')
     messages=messages.split()
+    breakpoint()
     for ele in messages:
         msg=mail.fetch(ele,'RFC822')
         raw=msg[1][0][1]
         raw_message=email.message_from_bytes(raw)
-        # result=email.message_from_string(raw_message)
-        # if result.is_multipart():
-        #     print("YES")
-        # else:
-        #     print("No")
         for part in raw_message.walk():
             if part.get_content_maintype() == 'multipart' or part.get('Content-Disposition') is None:
                 continue
@@ -38,7 +40,6 @@ def read_root():
                 filePath =os.path.join('/Users/abhishekraj/desktop/AI Business Automation Assistant/Downloaded Files',filename)
                 with open(filePath,'wb') as f:
                     f.write(part.get_payload(decode=True))
-                break
 
 
    
